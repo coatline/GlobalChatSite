c4491efeaafc223c897f61df8e3b4417b048b02d
BuffGruff <dylansr504@gmail.com>
2025-02-02 15:45:32 -0500

    1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let ClickButton = getElementById("ClickButton");
+let clickButton = getElementById("clickButton");
 let moneyLabelHTML = getElementById("moneyLabelHTML");
 let money = 0;
 let moneyPerClick = 1;
 
-ClickButton.onclick = function() {
+clickButton.onclick = function() {
     money += moneyPerClick;
-    moneyLabelHTML.textContent = money;
+    moneyLabelHTML.textContent = `$${money}`;
 }
