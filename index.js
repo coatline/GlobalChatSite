be56745c334d3023f43168ed3925560a32b3df57
BuffGruff <dylansr504@gmail.com>
2025-02-13 12:33:46 -0500

    cleaned up some code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //Can you add something that updates the money text content after buying something?
 //Add ripple effect when clicking the buttons (Ex. https://www.youtube.com/watch?v=Azp-IONVzfM)
 
-let buttonContainer = document.getElementById("buttonHolder")
+let buttonContainer = document.getElementById("buttonHolder");
 let clickButton = document.getElementById("clickButton");
 let moneyLabelHTML = document.getElementById("moneyLabelHTML");
 let moneyPerClick = 1;
@@ -52,35 +52,41 @@ class ClickGainer{
     }
 
     CreateUI(){
+        // Add the holder for this button.
         let wrapper = document.createElement("div");
         
-        wrapper.style.display = "flex"
+        wrapper.style.display = "flex";
         wrapper.style.gap = "10px";
-        wrapper.style.alignItems = "center"
+        wrapper.style.alignItems = "center";
         
-        buttonContainer.appendChild(wrapper)
+        // Put the holder for this button in the button container.
+        buttonContainer.appendChild(wrapper);
 
+        // Create the button
         let button = document.createElement("button");
         button.textContent = `${this.name} ($${this.cost})`;
         button.id = "clickButton";
         button.onclick = () => this.TryBuy();
         
+        // Create the label next to the button.
         let label = document.createElement("span");
         label.textContent = `+$${this.amountPerInterval} per ${this.interval / 1000}s`;
         label.id="label";
 
         wrapper.appendChild(button);
-        wrapper.appendChild(label)
+        wrapper.appendChild(label);
     }
 }
 
 let clickGainerTypes = {  }
 
-let gainer = new ClickGainer(5, 1000, 10, "Grandma", (255, 255, 255));
-let gainer1 = new ClickGainer(5, 1000, 10, "Grandpa", (255, 255, 255));
-let gainer2= new ClickGainer(500, 1000, 100, "Dad", (255, 255, 255));
-let gainer3 = new ClickGainer(5, 1000, 10, "Mom", (255, 255, 255));
-let gainer4 = new ClickGainer(5, 1000, 10, "Ya mum", (255, 255, 255));
+let gainerNames = [`Gain 1`, `Gain 2`, `Gain 3`, `Gain 4`, `Gain 5`]
+
+let gainer = new ClickGainer(5, 1000, 10, gainerNames[0], (255, 255, 255));
+let gainer1 = new ClickGainer(5, 1000, 10, gainerNames[1], (255, 255, 255));
+let gainer2= new ClickGainer(500, 1000, 100, gainerNames[2], (255, 255, 255));
+let gainer3 = new ClickGainer(5, 1000, 10, gainerNames[3], (255, 255, 255));
+let gainer4 = new ClickGainer(5, 1000, 10, gainerNames[4], (255, 255, 255));
 
 // Simulate Update being called every frame (assuming 60 FPS)
 setInterval(() => gainer.Update(1000 / 60), 1000 / 60);
