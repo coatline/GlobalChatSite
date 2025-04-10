43c57e5eef06c1709a6022485bad9b223c55b8c2
bryler <bryler.castle@gmail.com>
2025-04-10 13:04:45 -0400

    Started adding support for actually signing in with pocketbase

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,67 @@
+import PocketBase from 'https://cdn.jsdelivr.net/npm/pocketbase@0.18.3/+esm'
+
 const signInForm = document.getElementById('signInForm');
 const signInSubmitButton = document.getElementById('signInSubmitButton');
 
-signInSubmitButton.addEventListener('click', function (event) {
-    event.preventDefault(); // Prevent form submission
-    submit(); // Call the submit function
-});
+signInSubmitButton.addEventListener('click', trySignIn);
 
-function submit() {
+async function trySignIn(event)
+{
+    event.preventDefault();
+
+    const pb = new PocketBase('http://localhost:8090'); 
+
     const emailInput = document.getElementById('emailInput').value.trim();
     const passwordInput = document.getElementById('passwordInput').value.trim();
 
-    // Check if email and password are not empty
-    if (!emailInput || !passwordInput) {
+    if(isValidEmail(emailInput) == false)
+    {
+        // TODO: Display invalid email address
+        return;
+    }
+    else if(isValidPassword(passwordInput) == false)
+    {
+        // TODO: Display invalid password
         return;
     }
 
-    // Check if email is in a valid format
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(emailInput)) {
-        return;
+    const data = {
+      email: emailInput,
+      password: passwordInput
+    };
+
+    console.log(`Signing in... ${data.email} ${data.password}`);
+
+    try {
+      const user = await pb.collection('users').create(data);
+      console.log('Account created:', user);
+    } catch (err) {
+      console.error('Signup error:', err);
     }
+
+}
+
+function isValidEmail(email = String){
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return (emailRegex.test(emailInput) == false) && (email.length > 0);
+}
+
+function isValidPassword(password = String){
+    return password.length > 0;
+}
+
+function isvalidInputs() {
+
+    if (!emailInput || !passwordInput) {
+        return false;
+    }
+
+    
+
 
     window.location.href = '../html/chat.html';
+}
+
+async function signIn()
+{
 }
