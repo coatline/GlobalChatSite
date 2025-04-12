e4e2b1b68b799364bf677da18fbfefe6ff80a242
Dylan <dylansr504@gmail.com>
2025-04-12 14:34:48 -0400

    Created framework for profile creation

---
+++ public/js/createProfile.js
@@ -0,0 +1,5 @@
+const createProfileButton = document.getElementById('createProfileButton');
+
+createProfileButton.addEventListener('click', function() {
+    document.location.href = '../html/chat.html';
+});
